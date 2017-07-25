b9e2ac9466f144b6ca2d4b4106e5d1a6d23ece35
ktw1222 <ktw1222@gmail.com>
2017-07-25 15:27:37 -0400

    add Bookshelf.js

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -3,15 +3,13 @@ import React, { Component } from 'react'
 class ListBooks extends Component {
   render() {
     return (
-      <div className="bookshelf">
+      <div className="list-books">
         <div className="list-books-title">
-          <h1>{MyReads}</h1>
-          <div className="">
-
-          </div>
+          <h1>MyReads</h1>
         </div>
       </div>
-
     )
   }
 }
+
+export default ListBooks
