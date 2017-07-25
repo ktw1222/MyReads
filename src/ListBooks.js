4ada22c0d60717725d296b31af2002c1ad15d5d1
ktw1222 <ktw1222@gmail.com>
2017-07-25 15:08:01 -0400

    add SearchBooks.js

---
+++ src/ListBooks.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class ListBooks extends Component {
+  render() {
+    return (
+      <div className="bookshelf">
+        <div className="list-books-title">
+          <h1>{MyReads}</h1>
+          <div className="">
+
+          </div>
+        </div>
+      </div>
+
+    )
+  }
+}
