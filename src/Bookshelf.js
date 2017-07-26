2d1ce326fedb91d96ec5b974b002245aae2b5d5d
ktw1222 <ktw1222@gmail.com>
2017-07-26 10:42:07 -0400

    install react-router-dom

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -11,6 +11,22 @@ class Bookshelf extends Component {
             </li>
           </ol>
         </div>
+
+        <h2 className="bookshelf-title">Want To Read</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            <li>
+            </li>
+          </ol>
+        </div>
+
+        <h2 className="bookshelf-title">Read</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            <li>
+            </li>
+          </ol>
+        </div>
       </div>
     )
   }
